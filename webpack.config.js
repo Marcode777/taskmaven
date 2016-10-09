6aa3cf1bcb815d3a3855d4b6e354b3905eb25f96
Marcode777 <marcosebello7@gmail.com>
2016-10-09 18:35:27 -0400

    modified webpack.config.js, added .babelrc file, functional, able to display intended index.js test message

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,12 @@ module.exports = {
   module:{
     loaders:[
     {
-      test: /\.jsx?$/,
-      exlude:/node_module/,
-      loader:["react-hot", "babel?presets[]=react,presets[]=es2015"]
+      test: /\.jsx$/,
+      exlude:/node_modules/,
+      loaders:["babel-loader"]
     }
     ]
-  }.
+  },
   plugins:[
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
@@ -31,3 +31,11 @@ module.exports = {
 };
 
 // might have to double-check this, as this was all manually typed
+// had to change test: /\.jsx?$/ to test: /\.jsx$/ without the question mark
+// had to change loader:["react-hot", "babel?presets[]=react,presets[]=es2015"] to loaders:["babel-loader"]
+// had to create a new file called .babelrc and pin it to the main directory with presets: ["react", "es2015"], plugins: ["react-hot-loader/babel"], remembering to take the quotation marks away from presets and plugins
+// remember that to actually see the message at this particular point, you have to go into the URL http://localhost:8080/webpack-dev-server/bundle
+
+
+
+
